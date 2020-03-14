9788757cb4841cb2ac840ef04fd2d8ea6407c592
Evan <814422859@qq.com>
2020-03-14 22:53:35 +0800

    调用摄像头实时预览画面

--- app/src/main/java/lht/trash/MainActivity.java
+++ app/src/main/java/lht/trash/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final int PERMISSIONS_REQUEST = 1;
 
     //要使用viewmodel首先声明一个对象
     //MyViewModel myViewModel;
@@ -23,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
         //创建viewmodel对象
         //myViewModel= ViewModelProviders.of(this).get(MyViewModel.class);
 
+        //权限动态申请
+        if (hasPermission()) {
+            //nope
+        } else {
+            requestPermission();
+        }
+
+
+
+        //转到登录后的页面
         bt_login=findViewById(R.id.bt_login);
         bt_login.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -30,9 +47,29 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent =new Intent(MainActivity.this,funcActivity.class);
                 startActivity(intent);
 
-                //转到登录后的页面
             }
         });
 
     }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Camera permission is required for this demo",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+            requestPermissions(new String[] {PERMISSION_CAMERA}, PERMISSIONS_REQUEST);
+        }
+    }
+
 }
--- app/src/main/java/lht/trash/pic_searchFragment.java
+++ app/src/main/java/lht/trash/pic_searchFragment.java
@@ -1,12 +1,19 @@
 package lht.trash;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Rational;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,6 +22,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class pic_searchFragment extends Fragment {
+    TextureView textureView;
 
     public pic_searchFragment() {
         // Required empty public constructor
@@ -33,5 +41,57 @@ public class pic_searchFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         //getView().findViewById(R.id.bt_pic_to_text).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_pic_searchFragment_to_text_searchFragment));
         //getView().findViewById(R.id.bt_pic_to_voice).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_pic_searchFragment_to_voice_searchFragment));
+
+        textureView=getView().findViewById(R.id.textureView);
+
+        textureView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                updateTransform();
+            }
+        });
+        textureView.post(new Runnable(){
+
+            @Override
+            public void run() {
+                startCamera();
+            }
+        });
+    }
+    private void updateTransform(){
+        Matrix matrix = new Matrix();
+        float centerX=textureView.getWidth()/2f;
+        float centerY = textureView.getHeight() / 2f;
+
+        float[] rotations = {0,90,180,270};
+        // Correct preview output to account for display rotation
+        float rotationDegrees = rotations[textureView.getDisplay().getRotation()];
+
+        matrix.postRotate(-rotationDegrees, centerX, centerY);
+
+        // Finally, apply transformations to our TextureView
+        textureView.setTransform(matrix);
+    }
+    private void startCamera() {
+        // 1. preview
+        PreviewConfig previewConfig = new PreviewConfig.Builder()
+                .setTargetAspectRatio(new Rational(1, 1))
+                .setTargetResolution(new Size(640,640))
+                .build();
+
+        Preview preview = new Preview(previewConfig);
+        preview.setOnPreviewOutputUpdateListener(new Preview.OnPreviewOutputUpdateListener() {
+            @Override
+            public void onUpdated(Preview.PreviewOutput output) {
+                ViewGroup parent = (ViewGroup) textureView.getParent();
+                parent.removeView(textureView);
+                parent.addView(textureView, 0);
+
+                textureView.setSurfaceTexture(output.getSurfaceTexture());
+                updateTransform();
+            }
+        });
+
+        CameraX.bindToLifecycle(this, preview);
     }
 }
